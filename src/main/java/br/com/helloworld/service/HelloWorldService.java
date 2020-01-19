47ce82ad85f64e5410a5d6c9d2c8be732d23d312
Arthur Silva <fuscellaarthur@gmail.com>
2020-01-18 23:26:30 -0300

    HWSO:0004 - Adjusted unit tests

--- src/main/java/br/com/helloworld/service/HelloWorldService.java
+++ src/main/java/br/com/helloworld/service/HelloWorldService.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.helloworld.config.OAuth2Configuration;
-import br.com.helloworld.exception.OAuthException;
+import br.com.helloworld.exception.HelloWorldException;
 
 public class HelloWorldService {
 
@@ -16,11 +16,16 @@ public class HelloWorldService {
 		this.oAuth2Configuration = new OAuth2Configuration();
 	}
 
-	public void handleGetRequest(HttpServletRequest request, HttpServletResponse response)
-			throws OAuthException, IOException {
-		//this.oAuth2Configuration.checkAuthorization(request);
+	public void handleGetRequest(HttpServletRequest request, HttpServletResponse response) throws HelloWorldException {
+		this.oAuth2Configuration.checkAuthorization(request);
 
-		PrintWriter pw = response.getWriter();
+		PrintWriter pw;
+		try {
+			pw = response.getWriter();
+		}
+		catch (IOException e) {
+			throw new HelloWorldException();
+		}
 		pw.println("Contact light");
 	}
 }
